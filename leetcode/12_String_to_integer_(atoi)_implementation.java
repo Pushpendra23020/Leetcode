1648f66b87fc6b6ccbbf8ca31de1b28b8614682c
Pushpendra <pandeypushpendra189@gmail.com>
2025-07-23 18:53:22 +0530

    Add files via upload

---
+++ leetcode/12_String_to_integer_(atoi)_implementation.java
@@ -0,0 +1,4 @@
+//String to integer (atoi) implementation
+class Practise {
+    public static int myAtoi(String s) {
+        int i = 0, sign = 1, total = 0;
---
+++ leetcode/13_Skip_whitespace.java
@@ -0,0 +1,4 @@
+        // Skip whitespace
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
